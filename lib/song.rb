03ad5e61f3c3a3146fa9d37ef34a117b5fbf2277
Riley Moore <rilemoore@gmail.com>
2019-10-18 15:11:17 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -52,6 +52,20 @@ class Song
     genre_hash
   end
   
+    def self.artist_count
+    artist_hash = {}
+    @@artists.each do |artist|
+      
+      if(artist_hash.has_key?(artist))
+        artist_hash[artist] += 1
+      else
+        artist_hash[artist] = 1
+      end
+      
+    end
+    artist_hash
+  end
+  
 end
 
 
